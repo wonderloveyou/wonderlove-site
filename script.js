68f2bacdac92618db4f6f2f945c3add8751f6891
wonderloveyou <wdrlvbeats@gmail.com>
2025-04-15 18:44:45 +0700

    2.0v

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 document.getElementById("overlay").addEventListener("click", () => {
   const overlay = document.getElementById("overlay");
   const panel = document.getElementById("link-panel");
+  const clickSound = document.getElementById("click-sound");
+
+  if (clickSound) {
+    clickSound.volume = 0.7;
+    clickSound.play();
+  }
 
   overlay.classList.add("fade-out");
   setTimeout(() => {
